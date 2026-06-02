d4eeb157cef4669cedd5f9241b4ee1846b028a19
zohrehchenari <zohrehchenari2874@gmail.com>
2026-06-02 17:04:04 +0330

    add context and reducer on async action

--- src/components/context/BookmarkListContext.jsx
+++ src/components/context/BookmarkListContext.jsx
@@ -1,4 +1,10 @@
-import { createContext, useContext, useEffect, useReducer, useState } from "react";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useReducer,
+  useState,
+} from "react";
 // import { PiOpenAiLogo } from "react-icons/pi";
 import toast from "react-hot-toast";
 import axios from "axios";
@@ -7,71 +13,80 @@ import axios from "axios";
 const BookmarkContext = createContext();
 const BASE_URL = "http://localhost:5000";
 
-
 const initialState = {
   bookmarks: [],
   isLoader: false,
   currentBookmark: null,
   error: null,
-}
+};
 
-function bookmarkReducer (state, action){
-  switch(action.type){
-    case "loading": return {
+function bookmarkReducer(state, action) {
+  switch (action.type) {
+    case "loading":
+      return {
         ...state,
-         isLoader: true,
-    }
-    case"bookmarks/loaded": return {
-      ...state,
-      isLoader: false,
-      bookmarks: action.payload,
-    }
-    case"bookmark/loaded":{
-
-    }
-     case"bookmark/created":{
-
-    } 
-    case"bookmark/deleted":{
-
-    }
-    case"rejected": return{
-      ...state,
-       isLoader:false,
-       error: action.payload,
-    }
+        isLoader: true,
+      };
+    case "bookmarks/loaded":
+      return {
+        ...state,
+        isLoader: false,
+        bookmarks: action.payload,
+      };
+    case "bookmark/loaded":
+      return {
+        ...state,
+        isLoader: false,
+        currentBookmark: action.payload,
+      };
+    case "bookmark/created":
+      return {
+        ...state,
+        isLoader: false,
+        bookmarks: [...state.bookmarks, action.payload],
+        currentBookmark : action.payload,
+      };
+    case "bookmark/deleted":
+      return {
+        ...state,
+        isLoader: false,
+        bookmarks: state.bookmarks.filter((item) => item.id !== action.payload),
+        currentBookmark : null,
+      };
+    case "rejected":
+      return {
+        ...state,
+        isLoader: false,
+        error: action.payload,
+      };
     default:
-       throw new Error("unkhown action")
+      throw new Error("unkhown action");
   }
 }
 
-
-
-
 // BookmarkProvider COMPONENT FOR COVER OTHER COMPONENTS WITH TRANSMISSION VALUE IN CONTEXT...
 function BookmarkProvider({ children }) {
-  // STATE FOR GET CURRENTHOTE IN ROUTE bookmark....
-  // const [currentBookmark, setcurrentBookmark] = useState(null);
-  // STATE FOR CREATE ISlOADER UNIQUE FOR FETCHBOOKMARK....
-  // const [bookmarks, setBookmarks] = useState([]);
-  // const [isLoader, setIsLoader] = useState(false);
 
   // userReducer hooks.....
-   const [{bookmarks,isLoader, currentBookmark},dispath] = useReducer(bookmarkReducer, initialState);
+  const [{ bookmarks, isLoader, currentBookmark }, dispatch] = useReducer(
+    bookmarkReducer,
+    initialState,
+  );
 
-  
   // city and country add to db.json shod....for show update this data(get all bookmark)...
   useEffect(() => {
     async function fetchBookmarkList() {
-      dispath({type:"loading"});
+      dispatch({ type: "loading" });
 
       try {
         const { data } = await axios.get(`${BASE_URL}/bookmarks`);
-        dispath({type:"bookmarks/loaded" , payload:data})
+        dispatch({ type: "bookmarks/loaded", payload: data });
       } catch (error) {
         toast.error(error.message);
-      } finally {
-        setIsLoader(false);
+        dispatch({
+          type: "rejected",
+          payload: "an Error accurred in loading bookmark",
+        });
       }
     }
     fetchBookmarkList();
@@ -79,58 +94,49 @@ function BookmarkProvider({ children }) {
 
   // for get one bookmark....
   async function getBookmark(id) {
-    dispath({type: "loading"});
-    // setcurrentBookmark(null);
+    if(Number(id) === currentBookmark?.id) return
+    dispatch({ type: "loading" });
     try {
       const { data } = await axios.get(`${BASE_URL}/bookmarks/${id}`);
-      setcurrentBookmark(data);
+      dispatch({ type: "bookmark/loaded", payload: data });
       // console.log(data);
     } catch (error) {
       toast.error(error.message);
-      dispath({type: "rejected", payload:"an error accured in loading bookmark"});
-    } finally {
-      setIsLoader(false);
+      dispatch({
+        type: "rejected",
+        payload: "an error accured in fetching single bookmark",
+      });
     }
   }
 
   // create function for manage newBookmark object in AddNewBookmark component...(create bookmark)
   async function createBookmark(newBookmark) {
-    setIsLoader(true);
+    dispatch({ type: "loading" });
     try {
       const { data } = await axios.post(`${BASE_URL}/bookmarks/`, newBookmark);
 
       //for update and show currentbookmar in route=> /bookmark(bookmark list)...
-      setcurrentBookmark(data);
+      dispatch({ type: "bookmark/created", payload: data });
       // for show old bookmark city and new bookmark city...
-      setBookmarks((prev) => [...prev, data]);
-      setcurrentBookmark(data);
     } catch (error) {
       toast.error(error.message);
-    } finally {
-      setIsLoader(false);
+      dispatch({ type: "rejected", payload: error.message });
     }
   }
 
   // function for delete bookmarkList in route bookmark....
-   async function deleteBookmark(id) {
-    setIsLoader(true);
+  async function deleteBookmark(id) {
+    dispatch({ type: "loading" });
     try {
-
-    await axios.delete(`${BASE_URL}/bookmarks/${id}`);
+      await axios.delete(`${BASE_URL}/bookmarks/${id}`);
 
       // for show old bookmark city and new bookmark city...
-      setBookmarks((prev)=>prev.filter ((item)=>item.id !== id));
-     
+      dispatch({ type: "bookmark/deleted", payload: id });
     } catch (error) {
       toast.error(error.message);
-    } finally {
-      setIsLoader(false);
+      dispatch({ type: "rejected", payload: error.message });
     }
   }
-
-
-
-
 
   return (
     <BookmarkContext.Provider
@@ -140,7 +146,7 @@ function BookmarkProvider({ children }) {
         currentBookmark,
         getBookmark,
         createBookmark,
-        deleteBookmark
+        deleteBookmark,
       }}
     >
       {children}
@@ -154,3 +160,5 @@ export default BookmarkProvider;
 export function usebookmark() {
   return useContext(BookmarkContext);
 }
+
+// approuch : pass action
